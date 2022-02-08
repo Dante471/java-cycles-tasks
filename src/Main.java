6c58c6eb6ad1d335979d1b262ebfd09b4210bc94
dan-d <dan-dannn@yandex.ru>
2022-02-08 17:26:41 +0300

    Cycles tasks complete 2

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,10 @@ public class Main {
         int number = 1000;
         int divider = 4;
         int quantity = 5;
-        int iter = 0;
-        while (iter < quantity) {
+        for (int i = 0; i < quantity;) {
             if (number % divider == 0) {
                 System.out.print(number + " ");
-                iter++;
+                i++;
             }
             number++;
         }
@@ -28,11 +27,10 @@ public class Main {
         int number = 1;
         int divider = 2;
         int quantity = 10;
-        int iter = 0;
-        while (iter < quantity) {
+        for (int i = 0; i < quantity;) {
             if (number % divider != 0) {
                 System.out.print(number + " ");
-                iter++;
+                i++;
             }
             number++;
         }
@@ -41,12 +39,10 @@ public class Main {
 
     private static void task3() {
         int number = 90;
-        int divider = 5;
+        int deductible = 5;
         while (number > 0) {
-            if (number % divider == 0) {
-                System.out.print(number + " ");
-            }
-            number--;
+            System.out.print(number + " ");
+            number -= deductible;
         }
         System.out.println();
     }
@@ -55,11 +51,9 @@ public class Main {
         int number = 4;
         int multiplier = 2;
         int quantity = 10;
-        int iter = 0;
-        while (iter < quantity) {
+        for (int i = 0; i < quantity; i++) {
             System.out.print(number + " ");
             number *= multiplier;
-            iter++;
         }
         System.out.println();
     }
@@ -76,26 +70,29 @@ public class Main {
 
     private static void task6() {
         int luckyTicketsCount = 0;
-        int firstHalfSum = 0;
-        int secondHalfSum = 0;
-        for (int d1 = 0; d1 < 10; d1++) {
-            for (int d2 = 0; d2 < 10; d2++) {
-                for (int d3 = 0; d3 < 10; d3++) {
-                    for (int d4 = 0; d4 < 10; d4++) {
-                        for (int d5 = 0; d5 < 10; d5++) {
-                            for (int d6 = 0; d6 < 10; d6++) {
-                                firstHalfSum = d6 + d5 + d4;
-                                secondHalfSum = d3 + d2 + d1;
-                                if (firstHalfSum == secondHalfSum) {
-                                    luckyTicketsCount++;
-                                }
-                            }
-                        }
-                    }
-                }
+        int firstHalf;
+        int secondHalf;
+        int elementFirst;
+        int elementSecond;
+        for (int i = 1; i < 1000000; i++) {
+            firstHalf = i / 1000;
+            secondHalf = i - firstHalf * 1000;
+            elementFirst = firstHalf;
+            elementSecond = secondHalf;
+            int firstHalfSum = 0;
+            int secondHalfSum = 0;
+            while (elementFirst != 0) {
+                firstHalfSum += elementFirst % 10;
+                elementFirst /= 10;
+            }
+            while (elementSecond !=0) {
+                secondHalfSum += elementSecond % 10;
+                elementSecond /= 10;
+            }
+            if (firstHalfSum == secondHalfSum) {
+                luckyTicketsCount++;
             }
         }
-        luckyTicketsCount--; //ignoring ticket number 000000
         System.out.println("Number of lucky tickets per roll " + luckyTicketsCount);
     }
 
